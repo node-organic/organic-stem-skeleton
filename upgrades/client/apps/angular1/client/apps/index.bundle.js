5fb6d9a9325a25f038fb9efa136ae2057b896fa5
outbounder <obiwonn@gmail.com>
2015-12-13 21:04:23 +0200

    provide angular1 support

--- upgrades/client/apps/angular1/client/apps/index.bundle.js
+++ upgrades/client/apps/angular1/client/apps/index.bundle.js
@@ -1,8 +1,8 @@
 'use strict';
 
-require("../../../bower_components/angular/angular")
-require("../../../bower_components/angular-route/angular-route")
-require("../../../bower_components/angular-resource/angular-resource.js")
+require("../../bower_components/angular/angular")
+require("../../bower_components/angular-route/angular-route")
+require("../../bower_components/angular-resource/angular-resource.js")
 
 require("./controllers")
 require("./services")
@@ -17,6 +17,6 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {templateUrl: 'public/app/templates/landing.html', controller: 'LandingCtrl'});
+  $routeProvider.when('/', {templateUrl: '/public/templates/landing.html', controller: 'LandingCtrl'});
   $routeProvider.otherwise({redirectTo: '/404'});
 }]);
